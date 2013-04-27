994562c39e11181cd94534d5d556edf088a6ee1b
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-04-27 16:45:25 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git submodule init
 git submodule update
 
-ln -s ~/dotfile/vim ~/.vim
-ln -s ~/dotfile/vimrc ~/.vimrc
-ln -s ~/dotfile/bashrc ~/.bashrc
-ln -s ~/dotfile/gitconfig ~/.gitconfig
+ln -sf ~/dotfile/vim ~/.vim
+ln -sf ~/dotfile/vimrc ~/.vimrc
+ln -sf ~/dotfile/bashrc ~/.bashrc
+ln -sf ~/dotfile/gitconfig ~/.gitconfig

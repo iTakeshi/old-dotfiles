2cc4d52272263dace59509d1aa0763678465e54e
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-08-22 21:25:31 +0900

    bash_profile is under control

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ then
 fi
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/gemrc ~/.gemrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/Xmodmap ~/.Xmodmap

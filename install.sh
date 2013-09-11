e21328ad1c17fc4604dce2cf4275cf41ef0d6ce1
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-09-11 11:16:51 +0900

    add fonts

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ if [ ! -d ~/.vim ]
 then
   ln -sf ~/dotfiles/vim ~/.vim
 fi
+if [ ! -d ~/.fonts ]
+then
+  ln -sf ~/dotfiles/fonts ~/.fonts
+fi
+
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile

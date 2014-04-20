5822d0111d63c419376d7bebe02485d68752dccf
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-04-20 20:06:50 +0900

    .desktop files

--- install.sh
+++ install.sh
@@ -20,4 +20,12 @@ ln -sf ~/dotfiles/gemrc ~/.gemrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/Xmodmap ~/.Xmodmap
 
+ln -sf ~/dotfiles/applications/cjk-terminal.desktop ~/.local/share/applications/cjk-terminal.desktop
+ln -sf ~/dotfiles/applications/google-chrome.desktop ~/.local/share/applications/google-chrome-with-local-file-access.desktop
+
+if [ -e /usr/bin/skype ]
+then
+  ln -sf ~/dotfiles/applications/skype.desktop ~/.config/autostart/skype.desktop
+fi
+
 sh ~/dotfiles/gnome_terminal.sh

1e092b63cff993555fe7bfd437bf0f2e980ad621
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-02-12 22:01:59 +0900

    rename and clean first_time_setup.sh -> initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,71 @@
+# check pre-script setup
+cat <<EOT
+Complete these 2 steps BEFORE you run this script.
+
+  1. configure SSH connection to GitHub
+
+  2. execute following commands
+    $ sudo apt-get update
+    $ sudo apt-get dist-upgrade
+    $ sudo reboot
+
+Did you confirm? [Y/n]
+EOT
+read CONFIRMATION
+CONFIRMATION=`echo $CONFIRMATION | tr y Y | tr -d '[\[\]]'`
+case $CONFIRMATION in
+  "" | Y* ) echo "proceeding...";;
+  *       ) echo "aborted." && exit 0;;
+esac
+
+# use aptitude instead of apt-get
+sudo apt-get install aptitude
+
+# install fundamental tools
+sudo aptitude -y install \
+  build-essential curl autoconf automake lv python-software-properties sqlite3
+
+# install 3rd party softwares
+sudo sed -i -e "s/^# \(.* partner\)$/\1/g" /etc/apt/sources.list
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo apt-add-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo aptitude update
+sudo aptitude -y install \
+  google-chrome-stable git-core oracle-java8-installer \
+  nodejs dropbox python-gpgme libappindicator1 skype
+
+# clone dotfiles and apply
+cd ~
+git clone git@github.com:itakeshi/dotfiles.git --depth 1
+cd ~/dotfiles
+sh install_dotfiles.sh
+
+# install ruby and neovim
+cd ~/dotfiles
+sh install_ruby.sh
+sh install_neovim.sh
+
+# install linuxbrew
+sudo aptitude -y install \
+  build-essential curl m4 texinfo libbz2-dev libcurl4-openssl-dev \
+  libexpat-dev libncurses-dev zlib1g-dev
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+brew install caskroom/cask/brew-cask
+
+# install brew packages
+
+# other utility softwares
+sudo aptitude -y install asunder banshee ibus-mozc gimp inkscape rar
+
+# reboot
+cat <<EOT
+Your computer has been Successfully initialized! Yay!!
+Stroke any key to reboot.
+EOT
+read ANYKEY
+sudo reboot

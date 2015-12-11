61bd89a3c5b606db50c151c871f2ee0173034b9b
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-12-11 19:13:26 +0900

    .vimrc can be read from ~/.vim/vimrc

--- functions/install_dotfiles.sh
+++ functions/install_dotfiles.sh
@@ -18,7 +18,6 @@ then
   ln -sf ~/dotfiles/fonts ~/.fonts
 fi
 
-ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/gemrc ~/.gemrc

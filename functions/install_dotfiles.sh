6165a6c9c958f1f273d4afab0c87f8a99428ed92
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-12-12 12:06:45 +0900

    don't use skype on ubuntu no more

--- functions/install_dotfiles.sh
+++ functions/install_dotfiles.sh
@@ -26,9 +26,4 @@ ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/applications/cjk-terminal.desktop ~/.local/share/applications/cjk-terminal.desktop
 ln -sf ~/dotfiles/applications/google-chrome.desktop ~/.local/share/applications/google-chrome-with-local-file-access.desktop
 
-if [ -e /usr/bin/skype ]
-then
-  ln -sf ~/dotfiles/applications/skype.desktop ~/.config/autostart/skype.desktop
-fi
-
 sh ~/dotfiles/functions/gnome_terminal.sh

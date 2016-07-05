3d03e7756ba2da9f9566bcfa50af7083d6aad085
iTakeshi <takeshi.ito.doraemon@gmail.com>
2016-07-05 22:48:34 +0900

    renew initializing procedure

--- initialize.sh
+++ initialize.sh
@@ -20,17 +20,22 @@ case $CONFIRMATION in
   *       ) echo "aborted." && exit 0;;
 esac
 
-# use aptitude instead of apt-get
-sudo apt-get install aptitude
-
 # install fundamental tools
-sudo aptitude -y install \
+sudo apt-get -y install \
   build-essential curl autoconf automake lv python-software-properties sqlite3 \
   exuberant-ctags global compizconfig-settings-manager compiz-plugins-extra \
-  python3 python-pip python3-pip
-sudo pip install pygments
+  python3 python-pip python3-pip clang $(check-language-support) \
+  unity-tweak-tool nfs-common lua5.2 lua5.2-dev libperl-dev doxygen \
+  libffi-dev libgdbm-dev libreadline-dev libncurses-dev libyaml-dev \
+  libcurl4-openssl-dev zlib1g-dev libssl-dev libxml2-dev libxslt-dev sqlite3 \
+  libsqlite3-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev \
+  mysql-server mysql-client
+sudo pip install pygments mycli
 
-# install 3rd party softwares
+# use fcitx
+im-config -n fcitx
+
+# install 3rd-party softwares {{{
 sudo sed -i -e "s/^# \(.* partner\)$/\1/g" /etc/apt/sources.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
@@ -38,11 +43,12 @@ sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
 sudo apt-add-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:webupd8team/java
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo aptitude update
-sudo aptitude -y install \
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get update
+sudo apt-get -y install \
   google-chrome-stable git-core oracle-java8-installer \
-  nodejs dropbox python-gpgme libappindicator1 skype
+  nodejs dropbox python-gpgme libappindicator1
+# }}}
 
 # clone dotfiles and apply
 cd ~
@@ -50,17 +56,64 @@ git clone git@github.com:itakeshi/dotfiles.git --depth 1
 cd ~/dotfiles
 sh install_dotfiles.sh
 
-# install ruby, neovim, and gtags
-cd ~/dotfiles
-sh install_ruby.sh
-sh install_neovim.sh
-sh install_gtags.sh
+# install from source {{{
+if [ ! -d $HOME/usr/src ]
+then
+  mkdir -p $HOME/usr/src
+fi
+
+# ruby {{{
+cd $HOME/usr/src
+
+ruby_major=2
+ruby_minor=3
+ruby_teeny=1
+
+ruby_dir="ruby-${ruby_major}.${ruby_minor}.${ruby_teeny}"
+ruby_tar="${ruby_dir}.tar.gz"
+ruby_url="http://cache.ruby-lang.org/pub/ruby/${ruby_major}.${ruby_minor}/${ruby_tar}"
+
+wget $ruby_url
+tar -xf $ruby_tar
+rm $ruby_tar
+cd $ruby_dir
+./configure --prefix=$HOME/usr/
+make
+make install
+
+gem update --system
+gem update
+gem install bundler pry pry-byebug nokogiri
+# }}}
+
+# vim {{{
+cd $HOME/usr/src
+
+git clone https://github.com/vim/vim
+cd vim
+./configure --prefix=$HOME/usr/ --enable-fail-if-missing --enable-luainterp \
+    --enable-perlinterp --enable-pythoninterp --enable-python3interp \
+    --enable-rubyinterp --enable-cscope --enable-multibyte \
+    --disable-darwin --disable-selinux --with-x --with-gnome
+make
+make install
+# }}}
+
+# }}}
+
+# scala
+wget www.scala-lang.org/files/archive/scala-2.11.8.deb
+sudo dpkg -i scala-2.11.8.deb
+rm scala-2.11.8.deb
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install sbt
 
 # other utility softwares
-sudo aptitude -y install asunder banshee ibus-mozc gimp inkscape rar
+sudo apt-get -y install asunder banshee gimp inkscape rar
 
 # Use CapsLock key as extra Ctrl
-dconf reset /org/gnome/settings-daemon/plugins/keyboard/active
 dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
 
 # reboot
--- install.bat
+++
@@ -1,7 +0,0 @@
-git submodule init
-git submodule update
-
-mklink /j ..\vimfiles vim
-mklink /h ..\_vimrc vimrc
-
-mklink /h ..\.gitconfig gitconfig

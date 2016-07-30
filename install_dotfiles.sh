4efe3fe479e650f1bd4ace29336b9bbd11b2de46
iTakeshi <takeshi.ito.doraemon@gmail.com>
2016-07-30 15:36:56 +0900

    gnome terminal configuration

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -14,3 +14,7 @@ ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/gemrc ~/.gemrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+
+dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+
+dconf load "/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/" < terminal.dconf
